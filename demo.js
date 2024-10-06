ff6274ee8f138849aada5d20f77aa71acd880047
clsgon <hercarlos@hotmail.es>
2024-10-06 15:31:17 +0200

    demo finished

--- demo.js
+++ demo.js
@@ -34,7 +34,11 @@ scene.add(directionalLight);
 
 // Configurar el renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth / 3, window.innerHeight); // Ajustar el tamaño
+if (window.innerWidth > 480){
+  renderer.setSize(window.innerWidth / 3, window.innerHeight);
+} else {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+} // Ajustar el tamaño
 document.getElementById('globeViz').appendChild(renderer.domElement);
 
 // Añadir el globo y la luz a la escena
@@ -42,14 +46,18 @@ scene.add(Globe);
 scene.add(new THREE.AmbientLight(0xcccccc, Math.PI));
 
 // Configurar la cámara
-const camera = new THREE.PerspectiveCamera(75, (window.innerWidth / 3) / window.innerHeight, 0.1, 1000);
-camera.position.z = 300; // Ajusta la distancia según sea necesario
+var camera = null
+if (window.innerWidth > 480)
+  camera = new THREE.PerspectiveCamera(75, (window.innerWidth / 3) / window.innerHeight, 0.1, 1000);
+else
+  camera = new THREE.PerspectiveCamera(75, (window.innerWidth) / window.innerHeight, 0.1, 1000);
+camera.position.z = 298; // Ajusta la distancia según sea necesario
 camera.updateProjectionMatrix();
 
 // Controles de cámara
 const tbControls = new TrackballControls(camera, renderer.domElement);
-tbControls.minDistance = 250;
-tbControls.maxDistance = 350;
+tbControls.minDistance = 298;
+tbControls.maxDistance = 380;
 tbControls.rotateSpeed = 2;
 tbControls.zoomSpeed = 0.8;
 
@@ -69,8 +77,46 @@ tbControls.zoomSpeed = 0.8;
 
 // Ajustar el tamaño del renderizado al cambiar el tamaño de la ventana
 window.addEventListener('resize', () => {
-  const width = window.innerWidth / 3;
-  renderer.setSize(width, window.innerHeight);
+  const width = window.innerWidth;
+  if (width > 480){
+    renderer.setSize(width, window.innerHeight);
+  } else {
+    renderer.setSize(width / 3, window.innerHeight);
+  }
   camera.aspect = width / window.innerHeight;
   camera.updateProjectionMatrix();
+});
+
+
+document.getElementById("alert").style.display = "none"
+document.getElementById("destroy").style.display = "none"
+
+document.addEventListener("DOMContentLoaded", function() {
+  setTimeout(function() {
+    document.getElementById("alert").style.display = "";
+  }, 2000);
+
+  document.getElementById("alert").addEventListener("click", function () {
+    document.getElementById("alert").style.display = "none"
+    document.getElementById("icons").style.display = "none"
+    document.getElementById("asteroids").style.display = "block"
+    document.getElementById("asteroids").style.marginTop = "500px"
+    document.getElementById("destroy").style.display = ""
+    document.getElementById("destroy").style.marginTop = "100px"
+
+    const asteroids = document.querySelectorAll("#asteroids img");
+    for (let i = 0; i < asteroids.length; i += 1){
+      asteroids[i].addEventListener("click", function () {
+        asteroids[i].style.display = "none"
+        let counter = 0;
+        for (let j = 0; j < asteroids.length; j += 1){
+          if (asteroids[j].style.display == "none")
+            counter += 1;
+        }
+
+        if (counter == asteroids.length)
+          document.getElementById("destroy").innerHTML = "Congratulations!"
+      })
+    }
+  })
 });
